6e25727a45515e027c9543a112807cd5dd367ff1
DQNEO <dqneoo@gmail.com>
2015-03-05 22:52:53 +0900

    respect @Songmu !

--- samplecode/s3.pl
+++ samplecode/s3.pl
@@ -5,6 +5,7 @@ use warnings;
 use FindBin;
 use lib $FindBin::Bin . '/../lib';
 use Amazon::S3::Thin;
+use S3;
 use Config::Tiny;
 
 use Data::Dumper;
@@ -16,3 +17,9 @@ my $crd = Config::Tiny->read($config_file)->{$profile};
 warn Dumper $crd;
 
 
+package S3;
+
+# inspired by https://github.com/Songmu/App-LJ
+sub new_with_options {
+
+}
