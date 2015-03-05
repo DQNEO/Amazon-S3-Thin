a68ed9dcafa3ecbb56e783a9c53296d3721f6a2e
DQNEO <dqneoo@gmail.com>
2015-03-05 22:44:54 +0900

    read and show credentials

--- samplecode/s3.pl
+++ samplecode/s3.pl
@@ -7,6 +7,12 @@ use lib $FindBin::Bin . '/../lib';
 use Amazon::S3::Thin;
 use Config::Tiny;
 
+use Data::Dumper;
+my $profile = "dqneo";
+my $config_file = $ENV{HOME} . "/.aws/credentials";
 
+my $crd = Config::Tiny->read($config_file)->{$profile};
+
+warn Dumper $crd;
 
 
