12396459cda8f5458a6dfc7c1a367ee33ab635a6
DQNEO <dqneoo@gmail.com>
2018-08-21 01:37:21 +0900

    Checking in changes prior to tagging of version 0.21.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 2eea731..da0ea7b 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,9 @@ Revision history for Perl extension Amazon-S3-Thin
    
     {{$NEXT}}
    
    +0.21 2018-08-20T16:28:02Z
    +   - Made buildtool Makefile.PL not executable.
    +
     0.20 2018-08-20T15:12:52Z
        # Features
        - [Environment] Abandon perl 5.8 (break B.C.)

--- lib/Amazon/S3/Thin.pm
+++ lib/Amazon/S3/Thin.pm
@@ -9,7 +9,7 @@ use Encode;
 use Amazon::S3::Thin::Resource;
 use Amazon::S3::Thin::Credentials;
 
-our $VERSION = '0.20';
+our $VERSION = '0.21';
 
 my $METADATA_PREFIX      = 'x-amz-meta-';
 my $MAIN_HOST = 's3.amazonaws.com';
