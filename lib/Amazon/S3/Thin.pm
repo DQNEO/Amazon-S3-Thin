2b4f305709586df23648ccec8a8e77865147de0f
DQNEO <dqneoo@gmail.com>
2018-09-04 00:31:49 +0900

    Checking in changes prior to tagging of version 0.23.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 907dbdd..7ec5266 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,9 @@ Revision history for Perl extension Amazon-S3-Thin
    
     {{$NEXT}}
    
    +0.23 2018-09-03T15:30:25Z
    +   - Add Config::Tiny in PREREQ_PM of Makefile.PL to pass test
    +
     0.22 2018-08-22T14:34:37Z
        - Add more test and docs

--- lib/Amazon/S3/Thin.pm
+++ lib/Amazon/S3/Thin.pm
@@ -9,7 +9,7 @@ use Encode;
 use Amazon::S3::Thin::Resource;
 use Amazon::S3::Thin::Credentials;
 
-our $VERSION = '0.22';
+our $VERSION = '0.23';
 
 my $METADATA_PREFIX      = 'x-amz-meta-';
 my $MAIN_HOST = 's3.amazonaws.com';
